15cfb3d99fc5f25ce3aad271ecbe8bf96f549924
MarioDiCaprio <mariodicaprio10@gmail.com>
2023-01-30 23:36:57 +0100

    Updated sidebar

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,26 +1,75 @@
 import styles from "./Sidebar.module.sass";
-import React from "react";
+import React, {useState} from "react";
+import {motion} from "framer-motion";
 import {useMaxWidth} from "@/dimensions";
+import {GiKnifeFork as MenuIcon} from "react-icons/gi";
 
 
 const Sidebar: React.FC = () => {
     const isSmallScreen = useMaxWidth("small");
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+    function toggleMenuOpen() {
+        setIsMenuOpen(!isMenuOpen);
+    }
 
     if (isSmallScreen) {
         return (
-            <div className={styles.context}>
+            <motion.div className={styles.context}>
 
                 {/* Header (Title) */}
                 <div className={styles.header}>
                     <h1>Pizzeria bei Toni</h1>
                 </div>
 
-            </div>
+                <motion.div
+                    className={styles.menuButton}
+                    initial={undefined}
+                    animate={{ rotate: isMenuOpen? -180 : 0 }}
+                    onClick={toggleMenuOpen}
+                >
+                    <MenuIcon />
+                </motion.div>
+
+                <motion.div
+                    className={styles.menuContent}
+                    initial={undefined}
+                    animate={{ translateY: !isMenuOpen? '-100vh' : 0, opacity: !isMenuOpen? 0 : 1 }}
+                    transition={{ type: 'ease', duration: 0.5 }}
+                >
+                    {/* Address */}
+                    <div className={styles.address}>
+                        <a href="https://goo.gl/maps/WtdewpnyvgqJamHG6">
+                            Mathmeckestraße 4 <br />
+                            59889 Eslohe
+                        </a>
+                    </div>
+
+                    {/* Links */}
+                    <div className={styles.links}>
+                        <a>
+                            Fotos
+                        </a>
+                        <a>
+                            Menü
+                        </a>
+                        <a>
+                            Kontakt
+                        </a>
+                    </div>
+                </motion.div>
+
+            </motion.div>
         );
     }
 
     return (
-        <div className={styles.context}>
+        <motion.div
+            className={styles.context}
+            initial={{ translateX: -300, opacity: 0 }}
+            animate={{ translateX: 0, opacity: 1 }}
+            transition={{ type: 'ease', duration: 0.7 }}
+        >
 
             {/* Header (Title) */}
             <div className={styles.header}>
@@ -48,7 +97,7 @@ const Sidebar: React.FC = () => {
                 </a>
             </div>
 
-        </div>
+        </motion.div>
     );
 }
 
