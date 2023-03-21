2b81fbd58f0795e5495baceb07275e8a7681367c
MarioDiCaprio <mariodicaprio10@gmail.com>
2023-03-21 14:42:46 +0100

    Added @next/analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import '@/src/styles/globals.css'
 import type { AppProps } from 'next/app'
 import {CssBaseline, ThemeProvider} from "@mui/material";
 import {theme} from "@/src/theme";
+import {Analytics} from "@vercel/analytics/react";
 
 
 // noinspection JSUnusedGlobalSymbols
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider theme={theme}>
+            <Analytics mode="auto" />
             <CssBaseline />
             <Component {...pageProps} />
         </ThemeProvider>
